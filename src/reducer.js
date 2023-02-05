89f137d27755fa90d6bb655b2285847e6ae7887b
Pavel.K <pavelweb74@gmail.com>
2023-02-05 13:27:37 +0500

    add rootReducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,45 +1,9 @@
-import { ADD_TODO, STATUS_CHANGE, TOGGLE_TODO } from './constants'
-import { nextId } from './utils'
+import todosReducer from './features/todos/slice'
+import filtersReducer from './features/filters/slice'
 
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-  filters: {
-    status: 'ALL',
-    colors: [],
-  },
-}
-
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TODO: {
-      return {
-        ...state,
-        todos: [
-          ...state.todos,
-          { id: nextId(state.todos), text: action.payload, completed: false },
-        ],
-      }
-    }
-    case TOGGLE_TODO: {
-      return {
-        ...state,
-        todos: state.todos.map((todo) => {
-          const { id } = action.payload
-          if (todo.id === id) {
-            return { ...todo, completed: !todo.completed }
-          }
-          return todo
-        }),
-      }
-    }
-    case STATUS_CHANGE: {
-      return { ...state, filters: { ...state.filters, status: action.payload } }
-    }
-    default:
-      return state
+export default function rootReducer(state = {}, action) {
+  return {
+    todos: todosReducer(state.todos, action),
+    filters: filtersReducer(state.filters, action),
   }
 }
