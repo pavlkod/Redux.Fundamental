f298e47542cf661bbcce2117ea37d787cffdcca1
Pavel.K <pavelweb74@gmail.com>
2023-02-05 13:20:58 +0500

    add case in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,6 @@
+import { ADD_TODO, STATUS_CHANGE, TOGGLE_TODO } from './constants'
+import { nextId } from './utils'
+
 const initialState = {
   todos: [
     { id: 0, text: 'Learn React', completed: true },
@@ -12,6 +15,30 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_TODO: {
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: nextId(state.todos), text: action.payload, completed: false },
+        ],
+      }
+    }
+    case TOGGLE_TODO: {
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          const { id } = action.payload
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed }
+          }
+          return todo
+        }),
+      }
+    }
+    case STATUS_CHANGE: {
+      return { ...state, filters: { ...state.filters, status: action.payload } }
+    }
     default:
       return state
   }
