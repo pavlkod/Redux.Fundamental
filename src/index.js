49066b8c06fb6480e4b3fa5608ee74d948603122
Pavel.K <pavelweb74@gmail.com>
2023-02-05 19:48:08 +0500

    check dispatch logic

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ import App from './App'
 import './api/server'
 
 import store from './store'
+import { ADD_TODO, STATUS_CHANGE, TOGGLE_TODO } from './constants'
 
-console.log(store.getState())
+const { getState, dispatch, subscribe } = store
+console.log('Initial state: %O', getState())
+
+const unsubscribe = subscribe(() => {
+  console.log('State after dispatch: %O', getState())
+})
+
+const res = dispatch({ type: ADD_TODO, payload: 'text' })
+console.log(res)
+dispatch({ type: TOGGLE_TODO, payload: 1 })
+dispatch({ type: STATUS_CHANGE, payload: 'Active' })
+unsubscribe()
+
+dispatch({ type: STATUS_CHANGE, payload: 'Active2' })
 
 ReactDOM.render(
   <React.StrictMode>
