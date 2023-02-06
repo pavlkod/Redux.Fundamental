bb3e0e5103164754599675cbd89819e509014d6b
Pavel.K <pavelweb74@gmail.com>
2023-02-06 22:11:55 +0500

    add addTodosWithDelay middleware

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -1,14 +1,26 @@
+import { ADD_TODO } from 'constants'
+
 export const print1 = (storeAPI) => (next) => (action) => {
   console.log('1')
   return next(action)
 }
 
 export const print2 = (storeAPI) => (next) => (action) => {
-  console.log('2')
   return next(action)
 }
 
 export const print3 = (storeAPI) => (next) => (action) => {
   console.log('3')
+  return next(action)
+}
+
+export const addTodosWithDelay = (storeAPI) => (next) => (action) => {
+  console.log(action)
+  if (action.type === ADD_TODO) {
+    setTimeout(() => {
+      next(action)
+    }, 1_500)
+    return
+  }
   return next(action)
 }
