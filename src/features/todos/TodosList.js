6789d68546777759a51966dac03102573384d898
Pavel.K <pavelweb74@gmail.com>
2023-02-06 20:19:24 +0500

    Refactor to getIds for posts

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { shallowEqual, useSelector } from 'react-redux'
 import TodoItem from './TodoItem'
 
+const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
+
 const TodosList = () => {
-  const todos = []
-  const renderedTodos = todos.map((todo) => {
-    return <TodoItem key={todo.id} todo={todo} />
+  const todoIds = useSelector(selectTodoIds, shallowEqual)
+
+  const renderedTodos = todoIds.map((id) => {
+    return <TodoItem key={id} todoId={id} />
   })
   return <ul className="todo-list">{renderedTodos}</ul>
 }
