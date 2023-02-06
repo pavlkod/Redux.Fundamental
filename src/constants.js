d0da23e85083fe003f1329627d645609ae8d26dc
Pavel.K <pavelweb74@gmail.com>
2023-02-06 22:49:06 +0500

    add constants.js

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const ADD_TODO = 'todos/ADD_TODO'
+export const TOGGLE_TODO = 'todos/TOGGLE_TODO'
+export const REMOVE_TODO = 'todos/REMOVE_TODO'
+export const STATUS_CHANGE = 'filter/STATUS_CHANGE'
