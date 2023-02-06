3305215bde7fb569d91eaa0cf381702f72e4a132
Pavel.K <pavelweb74@gmail.com>
2023-02-06 23:11:46 +0500

    implement addTodo

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { add_todo } from '../todos/actions'
 
 const Header = () => {
+  const dispatch = useDispatch()
   const [text, setText] = useState('')
 
   const handleChange = (e) => setText(e.target.value)
+  const addTodo = (e) => {
+    e.preventDefault()
+    if (text.trim()) {
+      setText('')
+      dispatch(add_todo(text))
+    }
+  }
 
   return (
     <header className="header">
-      <input
-        className="new-todo"
-        placeholder="What needs to be done?"
-        value={text}
-        onChange={handleChange}
-      />
+      <form style={{ flex: 1 }} onSubmit={addTodo}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          value={text}
+          onChange={handleChange}
+        />
+      </form>
     </header>
   )
 }
