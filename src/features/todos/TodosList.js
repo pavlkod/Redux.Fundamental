ea745bf401929a5907cf7f813d7d8a7d3a9dc6e7
Pavel.K <pavelweb74@gmail.com>
2023-02-07 20:32:10 +0500

    add dispatch todos

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { shallowEqual, useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { fetchTodos } from './slice'
 import TodoItem from './TodoItem'
 
 const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
 
 const TodosList = () => {
+  const dispatch = useDispatch()
   const todoIds = useSelector(selectTodoIds, shallowEqual)
 
+  useEffect(() => {
+    dispatch(fetchTodos)
+  }, [dispatch])
+  // console.log(todoIds)
+  // let renderedTodos
   const renderedTodos = todoIds.map((id) => {
     return <TodoItem key={id} todoId={id} />
   })
