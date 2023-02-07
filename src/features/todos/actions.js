b008226221c0ec98618ac4198bf173d100d038a7
Pavel.K <pavelweb74@gmail.com>
2023-02-07 20:34:09 +0500

    add fetch_todos action_creator

--- src/features/todos/actions.js
+++ src/features/todos/actions.js
@@ -1,9 +1,15 @@
-import { ADD_TODO, TOGGLE_TODO } from '../../constants'
+import { ADD_TODO, FETCH_TODOS, TOGGLE_TODO } from '../../constants'
 
 export const add_todo = (text) => {
   return {
     type: ADD_TODO,
     payload: text,
+  }
+}
+export const fetch_todos = (todos) => {
+  return {
+    type: FETCH_TODOS,
+    payload: todos,
   }
 }
 export const toggle_todo = (id) => {
