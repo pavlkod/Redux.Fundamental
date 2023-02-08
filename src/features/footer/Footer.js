910af99c50362e12585f6cd2d145dba1b386f3ae
Pavel.K <pavelweb74@gmail.com>
2023-02-08 20:25:07 +0500

    refactoring Footer component

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,81 +1,22 @@
+import { statusChange } from 'features/filters/slice'
 import React from 'react'
-import { useSelector } from 'react-redux'
-
-import { availableColors, capitalize } from '../filters/colors'
-import { StatusFilters } from '../filters/slice'
+import { useDispatch, useSelector } from 'react-redux'
+import ColorFilters from './ColorFilters'
 
 import RemainingTodos from './RemainingTodos'
-
-const StatusFilter = ({ value: status, onChange }) => {
-  const renderedFilters = Object.keys(StatusFilters).map((key) => {
-    const value = StatusFilters[key]
-    const handleClick = () => onChange(value)
-    const className = value === status ? 'selected' : ''
-
-    return (
-      <li key={value}>
-        <button className={className} onClick={handleClick}>
-          {key}
-        </button>
-      </li>
-    )
-  })
-
-  return (
-    <div className="filters statusFilters">
-      <h5>Filter by Status</h5>
-      <ul>{renderedFilters}</ul>
-    </div>
-  )
-}
-
-const ColorFilters = ({ value: colors, onChange }) => {
-  const renderedColors = availableColors.map((color) => {
-    const checked = colors.includes(color)
-    const handleChange = () => {
-      const changeType = checked ? 'removed' : 'added'
-      onChange(color, changeType)
-    }
-
-    return (
-      <label key={color}>
-        <input
-          type="checkbox"
-          name={color}
-          checked={checked}
-          onChange={handleChange}
-        />
-        <span
-          className="color-block"
-          style={{
-            backgroundColor: color,
-          }}
-        ></span>
-        {capitalize(color)}
-      </label>
-    )
-  })
-
-  return (
-    <div className="filters colorFilters">
-      <h5>Filter by Color</h5>
-      <form className="colorSelection">{renderedColors}</form>
-    </div>
-  )
-}
+import StatusFilter from './StatusFilter'
 
 const Footer = () => {
   const { colors, status } = useSelector((state) => state.filters)
-  const todosRemaining = useSelector((state) => {
-    const todos = state.todos.filter((todo) => !todo.completed)
-    return todos.length
-  })
+  const dispatch = useDispatch()
 
-  const onColorChange = (color, changeType) =>
+  const onColorChange = (color, changeType) => {
     console.log('Color change: ', { color, changeType })
-  const onStatusChange = (status) => console.log('Status change: ', status)
-
-  console.log(colors, status)
+  }
+  const onStatusChange = (status) => {
+    console.log('Status change: ', status)
+    dispatch(statusChange(status))
+  }
 
   return (
     <footer className="footer">
@@ -85,7 +26,7 @@ const Footer = () => {
         <button className="button">Clear Completed</button>
       </div>
 
-      <RemainingTodos count={todosRemaining} />
+      <RemainingTodos />
       <StatusFilter value={status} onChange={onStatusChange} />
       <ColorFilters value={colors} onChange={onColorChange} />
     </footer>
