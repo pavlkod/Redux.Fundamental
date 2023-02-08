191afee49ad10113d4c9a1ab9b71f002c64dee76
Pavel.K <pavelweb74@gmail.com>
2023-02-08 20:25:49 +0500

    add useSelector in RemainingTodos component

--- src/features/footer/RemainingTodos.js
+++ src/features/footer/RemainingTodos.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-const RemainingTodos = ({ count }) => {
+const RemainingTodos = () => {
+  const count = useSelector((state) => {
+    const todos = state.todos.filter((todo) => !todo.completed)
+    return todos.length
+  })
   const suffix = count === 1 ? '' : 's'
 
   return (
