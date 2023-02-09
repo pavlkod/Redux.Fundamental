366bf8e6a8e9dbbeefbf8b179a26a94d27747dff
Pavel.K <pavelweb74@gmail.com>
2023-02-09 20:24:58 +0500

    refactoring in TodosList

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,22 +1,18 @@
 import React, { useEffect } from 'react'
-import { shallowEqual, useDispatch, useSelector } from 'react-redux'
-import { fetchTodos } from './slice'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTodos, selectTodoIds } from './slice'
 import TodoItem from './TodoItem'
-
-const selectTodoIds = (state) => state.todos.map((todo) => todo.id)
 
 const TodosList = () => {
   const dispatch = useDispatch()
-  const todoIds = useSelector(selectTodoIds, shallowEqual)
+  const todoIds = useSelector(selectTodoIds)
 
   useEffect(() => {
     dispatch(fetchTodos)
   }, [dispatch])
-  // console.log(todoIds)
-  // let renderedTodos
-  const renderedTodos = todoIds.map((id) => {
-    return <TodoItem key={id} todoId={id} />
-  })
+
+  const renderedTodos = todoIds.map((id) => <TodoItem key={id} todoId={id} />)
+
   return <ul className="todo-list">{renderedTodos}</ul>
 }
 
