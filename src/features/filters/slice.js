b219d61c5de302398fefed4aaa3d89838445425a
Pavel.K <pavelweb74@gmail.com>
2023-02-09 20:50:23 +0500

    add COLOR_CHANGE in filtersSlice

--- src/features/filters/slice.js
+++ src/features/filters/slice.js
@@ -1,4 +1,4 @@
-import { STATUS_CHANGE } from '../../constants'
+import { COLOR_CHANGE, STATUS_CHANGE } from '../../constants'
 
 export const StatusFilters = {
   All: 'all',
@@ -16,6 +16,17 @@ export default function reducer(state = initialState, action) {
     case STATUS_CHANGE: {
       return { ...state, status: action.payload }
     }
+    case COLOR_CHANGE: {
+      const { color, changeType } = action.payload
+      const colors = state.colors.slice()
+      if (changeType === 'added') {
+        colors.push(color)
+      } else {
+        const index = colors.indexOf(color)
+        colors.splice(index, 1)
+      }
+      return { ...state, colors: colors }
+    }
     default:
       return state
   }
@@ -25,5 +36,11 @@ export const statusChange = (status) => {
   return {
     type: STATUS_CHANGE,
     payload: status,
+  }
+}
+export const colorChange = (color, changeType) => {
+  return {
+    type: COLOR_CHANGE,
+    payload: { color, changeType },
   }
 }
