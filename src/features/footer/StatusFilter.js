39263edc14fcffad015500a75d71822221ff0ff5
Pavel.K <pavelweb74@gmail.com>
2023-02-10 20:49:11 +0500

    add StatusFilter component

---
+++ src/features/footer/StatusFilter.js
@@ -0,0 +1,27 @@
+import { StatusFilters } from 'features/filters/slice'
+import React from 'react'
+
+const StatusFilter = ({ value: status, onChange }) => {
+  const renderedFilters = Object.keys(StatusFilters).map((key) => {
+    const value = StatusFilters[key]
+    const handleClick = () => onChange(value)
+    const className = value === status ? 'selected' : ''
+
+    return (
+      <li key={value}>
+        <button className={className} onClick={handleClick}>
+          {key}
+        </button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="filters statusFilters">
+      <h5>Filter by Status</h5>
+      <ul>{renderedFilters}</ul>
+    </div>
+  )
+}
+
+export default StatusFilter
