fff0c67ae43ad531bd909dcb596696b8de6ef23d
Pavel.K <pavelweb74@gmail.com>
2023-02-10 21:18:36 +0500

    add CHANGE_COLOR_TODO in actions

--- src/features/todos/actions.js
+++ src/features/todos/actions.js
@@ -1,4 +1,9 @@
-import { ADD_TODO, FETCH_TODOS, TOGGLE_TODO } from '../../constants'
+import {
+  ADD_TODO,
+  CHANGE_COLOR_TODO,
+  FETCH_TODOS,
+  TOGGLE_TODO,
+} from '../../constants'
 
 export const add_todo = (text) => {
   return {
@@ -16,5 +21,11 @@ export const toggle_todo = (id) => {
   return {
     type: TOGGLE_TODO,
     payload: id,
+  }
+}
+export const change_color = (id, color) => {
+  return {
+    type: CHANGE_COLOR_TODO,
+    payload: { id, color },
   }
 }
