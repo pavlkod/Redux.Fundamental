b3dae263a7c069f0ef9d074aad32f9b7ea0b8531
Pavel.K <pavelweb74@gmail.com>
2023-02-11 21:38:17 +0500

    add LOADING_TODO constant

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ export const TOGGLE_TODO = 'todos/TOGGLE_TODO'
 export const REMOVE_TODO = 'todos/REMOVE_TODO'
 export const FETCH_TODOS = 'todos/FETCH_TODOS'
 export const CHANGE_COLOR_TODO = 'todos/CHANGE_COLOR_TODO'
+export const LOADING_TODO = 'todos/LOADING_TODO'
 
 export const STATUS_CHANGE = 'filter/STATUS_CHANGE'
 export const COLOR_CHANGE = 'filter/COLOR_CHANGE'
