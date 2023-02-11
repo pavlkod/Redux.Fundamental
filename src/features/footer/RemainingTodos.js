91116ca8acb52c111ff33d18be8da5a99abdeee8
Pavel.K <pavelweb74@gmail.com>
2023-02-11 18:27:05 +0500

    add selectTodos in RemainingTodos

--- src/features/footer/RemainingTodos.js
+++ src/features/footer/RemainingTodos.js
@@ -1,11 +1,11 @@
+import { selectTodos } from 'features/todos/slice'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
 const RemainingTodos = () => {
-  const count = useSelector((state) => {
-    const todos = state.todos.filter((todo) => !todo.completed)
-    return todos.length
-  })
+  const count = useSelector(
+    (state) => selectTodos(state).filter((todo) => !todo.completed).length
+  )
   const suffix = count === 1 ? '' : 's'
 
   return (
