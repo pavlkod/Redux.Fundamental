10d83fce95aa549aa9c848b884fdc04ef267ddeb
Pavel.K <pavelweb74@gmail.com>
2023-02-15 23:02:46 +0500

    connect to createSlice

--- src/features/todos/TodosList.js
+++ src/features/todos/TodosList.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchTodos, selectTodoIds } from './slice'
-import TodoItem from './TodoItem'
+// import { fetchTodos, selectTodoIds } from './slice'
+// import TodoItem from './TodoItem'
 
 const TodosList = () => {
   const dispatch = useDispatch()
-  const todoIds = useSelector(selectTodoIds)
+  // const todoIds = useSelector(selectTodoIds)
   const status = useSelector((state) => state.todos.status)
-
+  /*
   useEffect(() => {
     dispatch(fetchTodos)
   }, [dispatch])
+  */
+
+  const renderedTodos = ''
 
   if (status === 'loading') {
     return (
@@ -19,8 +22,10 @@ const TodosList = () => {
       </div>
     )
   }
-
+  /*
+  const todoIds = []
   const renderedTodos = todoIds.map((id) => <TodoItem key={id} todoId={id} />)
+  */
 
   return <ul className="todo-list">{renderedTodos}</ul>
 }
